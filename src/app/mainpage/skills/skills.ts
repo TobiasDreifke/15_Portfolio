2c2b28032118731a8366af69b03fcfa5bf7920ca
Tobias Dreifke <toobsecret@gmail.com>
2025-08-25 15:03:37 +0200

    created basics for skills component

--- src/app/mainpage/skills/skills.ts
+++ src/app/mainpage/skills/skills.ts
@@ -1,11 +1,58 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.scss'
 })
 export class Skills {
 
+  skillSet = [
+    {
+      language: "HTML",
+      icon: "html-icon.png",
+    },
+    {
+      language: "CSS",
+      icon: "html-icon.png",
+    },
+    {
+      language: "JavaScript",
+      icon: "html-icon.png",
+    },
+    {
+      language: "Material Design",
+      icon: "html-icon.png",
+    },
+    {
+      language: "TypeScript",
+      icon: "html-icon.png",
+    },
+    {
+      language: "Angular",
+      icon: "html-icon.png",
+    },
+    {
+      language: "Firebase",
+      icon: "html-icon.png",
+    },
+    {
+      language: "Git",
+      icon: "html-icon.png",
+    },
+       {
+      language: "REST-API",
+      icon: "html-icon.png",
+    },
+    {
+      language: "Scrum",
+      icon: "html-icon.png",
+    },
+    {
+      language: "Growth mindset",
+      icon: "html-icon.png",
+    },
+  ]
 }
