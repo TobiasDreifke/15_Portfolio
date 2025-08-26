1fdc1c1c606f692e1625970115aa7e6ddf34f7f7
Tobias Dreifke <toobsecret@gmail.com>
2025-08-26 14:22:29 +0200

    added language switch

--- src/app/shared-pages/navbar/navbar.ts
+++ src/app/shared-pages/navbar/navbar.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  activeLanguage: 'EN' | 'DE' = 'EN';  // default language
 
+  toggleLang() {
+    this.activeLanguage = this.activeLanguage === 'EN' ? 'DE' : 'EN';
+  }
 }
