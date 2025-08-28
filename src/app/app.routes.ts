b74783a091e7c3d43555385bacd3b59bacb26fdd
Tobias Dreifke <toobsecret@gmail.com>
2025-08-28 14:23:43 +0200

    routing enabled

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Mainpage } from './mainpage/mainpage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Mainpage },
+];
