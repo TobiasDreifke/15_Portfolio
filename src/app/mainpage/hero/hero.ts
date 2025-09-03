fe3737973d483eb781816b3800c7c6aed3bd11c9
Tobias Dreifke <toobsecret@gmail.com>
2025-09-03 10:32:33 +0200

    added a fade shadow and corrected the styling

--- src/app/mainpage/hero/hero.ts
+++ src/app/mainpage/hero/hero.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Banner } from "./banner/banner";
+import { TopElements } from './top-elements/top-elements';
 
 @Component({
   selector: 'app-hero',
-  imports: [Banner],
+  imports: [Banner, TopElements],
   templateUrl: './hero.html',
   styleUrl: './hero.scss'
 })
