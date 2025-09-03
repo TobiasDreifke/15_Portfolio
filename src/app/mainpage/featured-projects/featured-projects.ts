8c04b2b44c459f3ecde87e74b8aca8141df457a5
Tobias Dreifke <toobsecret@gmail.com>
2025-09-03 10:34:33 +0200

    Revert "added a fade shadow and corrected the styling"
    This reverts commit fe3737973d483eb781816b3800c7c6aed3bd11c9.

--- src/app/mainpage/featured-projects/featured-projects.ts
+++ src/app/mainpage/featured-projects/featured-projects.ts
@@ -1,37 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-featured-projects',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './featured-projects.html',
   styleUrl: './featured-projects.scss'
 })
 export class FeaturedProjects {
-  hoveredIndex: number = -1;
 
-  projects = [
-    {
-      name: 'Join',
-      tech: 'Angular | Typescript | HTML | CSS | Firebase',
-      image: 'assets/img/img/projects/Rectangle 30-1.png'
-    },
-    {
-      name: 'El Pollo Loco',
-      tech: 'HTML | CSS | JavaScript',
-      image: 'assets/img/img/projects/Rectangle 30-2.png'
-    },
-    {
-      name: 'DA Bubble',
-      tech: 'Angular | Firebase | Typescript',
-      image: 'assets/img/img/projects/Rectangle 30.png'
-    }
-  ];
-
-  getImageTop(index: number): string {
-    const totalRows = this.projects.length;
-    const percentages = [0, 20, 60]; 
-    return percentages[index] + '%';
-  }
 }
-
--- src/app/mainpage/hero/hero.ts
+++ src/app/mainpage/hero/hero.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { Banner } from "./banner/banner";
-import { TopElements } from './top-elements/top-elements';
 
 @Component({
   selector: 'app-hero',
-  imports: [Banner, TopElements],
+  imports: [Banner],
   templateUrl: './hero.html',
   styleUrl: './hero.scss'
 })
