8f84f2b3d754ae61c68a51d2d3a70d21188b03cd
Tobias Dreifke <toobsecret@gmail.com>
2025-09-04 15:34:13 +0200

    fake infinity loop

---
+++ src/app/data/testimonials.data.ts
@@ -0,0 +1,17 @@
+  import { Testimonial } from '../interfaces/testimonial.interface';
+
+  
+  export const TESTIMONIALS: Testimonial[] = [
+    {
+      text: 'Lukas is an amazing collaborator. His problem-solving skills are top-notch.',
+      author: 'H. Janisch – Team Partner'
+    },
+    {
+      text: 'Working with Lukas is a pleasure. He delivers quality work consistently.',
+      author: 'M. Schmidt – Project Lead'
+    },
+    {
+      text: 'Lukas always goes the extra mile to ensure the team succeeds.',
+      author: 'S. Keller – Colleague'
+    }
+  ];
--- src/app/mainpage/testimonials/testimonials.ts
+++ src/app/mainpage/testimonials/testimonials.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SingleTestimonial } from "./single-testimonial/single-testimonial";
 import { Testimonial } from '../../interfaces/testimonial.interface';
 import { CommonModule } from '@angular/common';
+import { TESTIMONIALS } from '../../data/testimonials.data';
 
 
 
@@ -12,31 +13,33 @@ import { CommonModule } from '@angular/common';
   styleUrl: './testimonials.scss'
 })
 export class Testimonials {
-  testimonials: Testimonial[] = [
-    {
-      text: 'Lukas is an amazing collaborator. His problem-solving skills are top-notch.',
-      author: 'H. Janisch – Team Partner'
-    },
-    {
-      text: 'Working with Lukas is a pleasure. He delivers quality work consistently.',
-      author: 'M. Schmidt – Project Lead'
-    },
-    {
-      text: 'Lukas always goes the extra mile to ensure the team succeeds.',
-      author: 'S. Keller – Colleague'
-    }
-  ];
+
+
+  originalTestimonials: Testimonial[] = TESTIMONIALS;
+  testimonials: Testimonial[] = [];
 
   currentIndex = 0;
   isTransitioning = true;
 
+  constructor() {
+
+    this.testimonials = [
+      ...this.originalTestimonials,
+      ...this.originalTestimonials,
+      ...this.originalTestimonials
+    ];
+
+
+    this.currentIndex = this.originalTestimonials.length;
+  }
+
+
   next() {
-    this.currentIndex++;
+    this.currentIndex = (this.currentIndex + 1) % this.testimonials.length;
   }
 
   prev() {
-    this.currentIndex--;
-    
+    this.currentIndex = (this.currentIndex - 1 + this.testimonials.length) % this.testimonials.length;
   }
 
   getTransform(): string {
