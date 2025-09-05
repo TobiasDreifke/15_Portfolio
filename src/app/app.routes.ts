aef3a6221ca7c9881abaaaae629b8d86607c017a
Tobias Dreifke <toobsecret@gmail.com>
2025-09-05 11:58:27 +0200

    contact form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Mainpage } from './mainpage/mainpage';
+import { LegalNotice } from './imprint/legal-notice/legal-notice';
 
 export const routes: Routes = [
     { path: '', component: Mainpage },
+    { path: 'legal', component: LegalNotice },
+     { path: 'about', component: LegalNotice },
 ];
