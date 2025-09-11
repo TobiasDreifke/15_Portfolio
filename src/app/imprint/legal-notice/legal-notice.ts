b09a8ef42a6b3f91169c0777c41403919540abfd
Tobias Dreifke <toobsecret@gmail.com>
2025-09-11 13:19:34 +0200

    legal notice translation

--- src/app/imprint/legal-notice/legal-notice.ts
+++ src/app/imprint/legal-notice/legal-notice.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [RouterModule, TranslatePipe],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
