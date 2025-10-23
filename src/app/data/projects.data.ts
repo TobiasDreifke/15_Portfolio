61578aeeeff7652551e6db0febc89b424055212d
Tobias Dreifke <toobsecret@gmail.com>
2025-10-23 11:58:40 +0200

    created a new project entry

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -11,26 +11,45 @@ export const PROJECTS: Projects[] = [
       { tech: 'CSS', image: 'assets/img/icons/css.png' },
       { tech: 'Firebase', image: 'assets/img/icons/firebase.png' }
     ],
-    image: 'assets/img/img/projects/Rectangle 30-2.png'
+    image: 'assets/img/img/projects/join.png',
+    github: "https://github.com/TobiasDreifke/join",
+    liveTest: "https://join.tobiasdreifke.com/",
   },
   {
-    nameKey: 'projects.elpollo.name',
-    descriptionKey: 'projects.elpollo.description',
+    nameKey: 'projects.knighthood.name',
+    descriptionKey: 'projects.knighthood.description',
     tech: [
       { tech: 'HTML', image: 'assets/img/icons/html.png' },
       { tech: 'CSS', image: 'assets/img/icons/css.png' },
       { tech: 'JavaScript', image: 'assets/img/icons/javascript.png' }
     ],
-    image: 'assets/img/img/projects/Rectangle 30-1.png'
+    image: 'assets/img/img/projects/knighthood.png',
+    github: "https://github.com/TobiasDreifke/knighthood",
+    liveTest: "https://knighthood.tobiasdreifke.com/",
   },
   {
-    nameKey: 'projects.dabubble.name',
-    descriptionKey: 'projects.dabubble.description',
+    nameKey: 'projects.pokedex.name',
+    descriptionKey: 'projects.pokedex.description',
     tech: [
-      { tech: 'Angular', image: 'assets/img/icons/angular.png' },
-      { tech: 'Firebase', image: 'assets/img/icons/firebase.png' },
-      { tech: 'Typescript', image: 'assets/img/icons/typescript.png' }
+      { tech: 'Javascript', image: 'assets/img/icons/javascript.png' },
+      { tech: 'HTML', image: 'assets/img/icons/html.png' },
+      { tech: 'CSS', image: 'assets/img/icons/css.png' }
     ],
-    image: 'assets/img/img/projects/Rectangle 30.png'
+    image: 'assets/img/img/projects/pokedex.png',
+    github: "https://github.com/TobiasDreifke/08_Pokedex",
+    liveTest: "https://pokedex.tobiasdreifke.com/",
   }
 ];
+
+// {
+//   nameKey: 'projects.dabubble.name',
+//   descriptionKey: 'projects.dabubble.description',
+//   tech: [
+//     { tech: 'Angular', image: 'assets/img/icons/angular.png' },
+//     { tech: 'Firebase', image: 'assets/img/icons/firebase.png' },
+//     { tech: 'Typescript', image: 'assets/img/icons/typescript.png' }
+//   ],
+//   image: 'assets/img/img/projects/Rectangle 30.png',
+//   github: "https://github.com/TobiasDreifke/join",
+//   liveTest: "https://join.tobiasdreifke.com/",
+// },
